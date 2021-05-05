77a0f947528a3cdf596057794bdceb49eb9f499e
Harshal <60959575+Harshal8887@users.noreply.github.com>
2021-05-05 12:34:35 +0530

    email validations added

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", boolean=True)
+    data = request.form
+    print(data)
+    return render_template("login.html")
 
 
 @auth.route('/logout')
@@ -13,6 +15,23 @@ def logout():
     return render_template("home.html")
 
 
-@auth.route('/signup')
-def sifn_up():
+@auth.route('/signup', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Invalid Email', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Passwords must be atleast 7 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
+
     return render_template("signin.html")
