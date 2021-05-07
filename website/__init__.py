8b6495bec0d82455383cd33be12b3c959d28f59e
Harshal <60959575+Harshal8887@users.noreply.github.com>
2021-05-07 20:07:11 +0530

    login manager

--- website/__init__.py
+++ website/__init__.py
@@ -13,6 +13,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'thequickbrownfox12133234577645'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+
     from .views import views
     from .auth import auth
 
@@ -22,6 +23,14 @@ def create_app():
     from .models import User, Note
 
     create_databse(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -16,6 +17,7 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in Succesfully', category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
 
             else:
@@ -27,8 +29,10 @@ def login():
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return render_template("home.html")
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/signup', methods=['GET', 'POST'])
@@ -58,6 +62,8 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             flash('Account created!', category='success')
+            login_user(user, remember=True)
+
             return redirect(url_for('views.home'))
 
     return render_template("signin.html")
